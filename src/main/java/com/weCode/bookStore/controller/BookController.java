9252e8bcfe036013bece33ce79e2016e4c269b18
losorio1 <losorio1@k12.com>
2022-10-12 10:24:04 -0500

    My first mistake in git

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks(){
+            BookDto book = BookDto.builder()
+                    .title("My First Book title")
+                    .build();
+
+        BookDto book2 = BookDto.builder()
+                .title("My Second Book title")
+                .build();
+
+            List<BookDto> books = new ArrayList<>();
+            books.add(book);
+            books.add(book2);
+            return ResponseEntity.ok(books);
+    }
+
+}
