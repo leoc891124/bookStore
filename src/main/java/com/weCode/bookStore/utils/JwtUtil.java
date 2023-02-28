ab2dd600e8c4d8f0ec951115d6306ead812a2408
losorio1 <losorio1@k12.com>
2023-02-28 18:36:15 -0500

    Book store backend V1.0

--- src/main/java/com/weCode/bookStore/utils/JwtUtil.java
+++ src/main/java/com/weCode/bookStore/utils/JwtUtil.java
@@ -22,7 +22,7 @@ public class JwtUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
 
-    public static final long JWT_TOKEN_VALIDITY = 30 * 60 * 2;
+    public static final long JWT_TOKEN_VALIDITY = 30 * 60 *2;
 
     @Value("${jwt:secret}")
     private String secret;
--- src/test/java/com/weCode/bookStore/service/UsuarioServiceTest.java
+++ src/test/java/com/weCode/bookStore/service/UsuarioServiceTest.java
@@ -1,4 +1,37 @@
 package com.weCode.bookStore.service;
 
+import com.weCode.bookStore.dto.UserDto;
+import com.weCode.bookStore.model.Usuario;
+import com.weCode.bookStore.repository.UsuarioRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
+
+   /* @InjectMocks
+    UserService userService;
+
+    @Mock
+    ModelMapper modelMapper;
+
+    @Mock
+    UsuarioRepository usuarioRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    public UUID addUser(UserDto userDto){
+        Usuario user = modelMapper.map(userDto, Usuario.class);
+
+        user.setPassword();
+    }*/
+
+
 }
